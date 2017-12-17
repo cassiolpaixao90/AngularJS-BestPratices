982cedac468e24c5ab987b93c4e1b9673a51292d
Cássio Paixão <cassiopaixaoti@gmail.com>
2017-12-17 14:29:46 -0200

    criacao do servidor node

---
+++ src/server/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  people: getPeople()
+};
+
+function getPeople() {
+  return [
+    { id: 1, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+    { id: 2, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+    { id: 3, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+    { id: 4, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+    { id: 5, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+    { id: 6, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+    { id: 7, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+    { id: 8, firstName: 'teste', lastName: 'teste', age: 25, location: 'teste' },
+  ];
+}
